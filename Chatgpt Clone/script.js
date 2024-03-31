1e0af0d7e1b58326cdb1278c57a1ea757335a56f
Tanisha3404 <115703082+Tanisha3404@users.noreply.github.com>
2024-03-31 22:30:04 +0530

    add

---
+++ Chatgpt Clone/script.js
@@ -0,0 +1,152 @@
+const chatInput = document.querySelector("#input-chat");
+const sendButton = document.querySelector("#send-btn");
+const chatContainer = document.querySelector(".chat-contianer");
+const themeButton = document.querySelector("#theme-btn");
+const deleteButton = document.querySelector("#delete");
+
+const API_KEY = "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"; 
+const chatboxHeight = chatInput.scrollHeight;
+let userText = null;
+
+const loadDataFromLocalStorage = () => {
+    const themeMode = localStorage.getItem("theme-mode");
+
+    document.body.classList.toggle("light-mode", themeMode === "light_mode");
+    themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
+    
+    // showing default text when chat is empty
+    const defaultText = `<div class="default-text">
+                            <h1>ChatGPT</h1>
+                            <p>ChatGPT Clone is a simple and interactive chatbot powered by AI. <br>
+                            Feel free to ask questions, share thoughts, or engage in a conversation.</p>
+                        </div>`
+
+    chatContainer.innerHTML = localStorage.getItem("all-chats") || defaultText;
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+}
+loadDataFromLocalStorage();
+
+const createElement = (html, className) => {
+    const chatDiv = document.createElement("div");
+    chatDiv.classList.add("chat", className);
+    chatDiv.innerHTML = html;
+    return chatDiv;
+}
+
+const getChatResponse = async(incomingChatDiv) => {
+    const API_URL = "https://api.openai.com/v1/completions";
+    const pElement = document.createElement("p");
+
+    // Properties & data for API request
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo-instruct",
+            prompt: userText,
+            max_tokens: 2048,
+            temperature: 0,
+            n: 1,
+            stop: null
+        })
+    }
+
+    // sending post request to API to get responses 
+    try {
+        const response = await (await fetch(API_URL, requestOptions)).json();
+        pElement.textContent = response.choices[0].text.trim();
+    } catch(error) {
+        pElement.classList.add("error");
+        pElement.textContent = "Oops! Something went wrong. Please try again.";
+    }
+
+    incomingChatDiv.querySelector(".typing-animation").remove();
+    incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+
+    // storing chats on local storage
+    localStorage.setItem("all-chats", chatContainer.innerHTML);
+}
+
+const copyResponse = (copyBtn) => {
+    const responseTextElement = copyBtn.parentElement.querySelector("p");
+    navigator.clipboard.writeText(responseTextElement.textContent);
+    copyBtn.textContent = "done";
+    setTimeout(() => copyBtn.textContent = "content_copy", 1000);
+} 
+
+const showTypingAnimation = () => {
+    const html = `<div class="chat-content">
+                    <div class="chat-details">
+                        <img src="images/chatgpt.jpg" alt="chatgpt-image">
+                        <div class="typing-animation">
+                            <div class="typing-dot" style="--delay: 0.2s"></div>
+                            <div class="typing-dot" style="--delay: 0.3s"></div>
+                            <div class="typing-dot" style="--delay: 0.4s"></div>
+                        </div>
+                    </div>
+                    <span onclick="copyResponse(this)" class="material-symbols-rounded">content_copy</span>
+                </div>`;
+
+    const incomingChatDiv = createElement(html, "incoming");
+    chatContainer.appendChild(incomingChatDiv);
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+    getChatResponse(incomingChatDiv);
+}
+
+const handleOutgoingChat = () => {
+    userText = chatInput.value.trim();
+    if(!userText) return;  // if input is empty
+
+    chatInput.value = "";
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+
+    const html = `<div class="chat-content">
+                    <div class="chat-details">
+                        <img src="images/user.jpg" alt="user-image" width="90" height="90">
+                        <p></p>
+                    </div>
+                </div>`;
+
+    const outgoingChatDiv = createElement(html, "outgoing");
+    outgoingChatDiv.querySelector("p").textContent = userText;
+
+    // removing default text when we enter something
+    document.querySelector(".default-text")?.remove();
+
+    chatContainer.appendChild(outgoingChatDiv);
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+    setTimeout(showTypingAnimation, 500);
+}
+
+themeButton.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+    localStorage.setItem("theme-mode", themeButton.innerText);
+    themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
+});
+
+deleteButton.addEventListener("click", () => {
+    if(confirm("Are you sure you want to delete chats?")) {
+        localStorage.removeItem("all-chats");
+        loadDataFromLocalStorage();
+    }
+});
+
+// adjusting height of chatbox dynamically
+chatInput.addEventListener("input", () => {
+    chatInput.style.height = `${chatboxHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+
+chatInput.addEventListener("keys", (e) => {
+    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleOutgoingChat();
+    }
+});
+
+sendButton.addEventListener("click", handleOutgoingChat);
