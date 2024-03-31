21b7cf46f778e35b4be8e444243fbad3ca083275
Tanisha3404 <115703082+Tanisha3404@users.noreply.github.com>
2024-03-31 22:23:49 +0530

    add script.js

--- Tic-tac-toe/script.js
+++ Tic-tac-toe/script.js
@@ -3,6 +3,7 @@ let popupRef = document.querySelector(".popup");
 let newgameBtn = document.getElementById("new-game");
 let restartBtn = document.getElementById("restart");
 let msgRef = document.getElementById("message");
+
 //Winning Pattern Array
 let winningPattern = [
   [0, 1, 2],
